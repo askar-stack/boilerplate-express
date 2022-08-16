d9106e2a5d1ca2b57d8a763c911c95400bc518d9
Yasmin Askar <hailey.askar@gmail.com>
2022-08-16 04:31:01 -0500

    sendFile

--- myApp.js
+++ myApp.js
@@ -1,7 +1,14 @@
 let express = require("express");
 let app = express();
 console.log("Hello World");
+/*
 app.get("/", function (request, response) {
   response.send("Hello Express");
+});
+*/
+let absolutePath = __dirname + relativePath / file.ext;
+
+app.get("/", (req, res) => {
+  res.sendFile(absolutePath);
 });
 module.exports = app;
