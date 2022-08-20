0dad50c42856fd793d734a8fe746369d4f6d1a37
Yasmin Askar <hailey.askar@gmail.com>
2022-08-20 03:43:27 -0500

    serve static files

--- myApp.js
+++ myApp.js
@@ -6,9 +6,15 @@ app.get("/", function (request, response) {
   response.send("Hello Express");
 });
 */
+
 let absolutePath = __dirname + "/views/index.html";
 
 app.get("/", (req, res) => {
   res.sendFile(absolutePath);
 });
+
+const absPath = __dirname + "/public";
+
+app.use("/", express.static(absPath));
+
 module.exports = app;
