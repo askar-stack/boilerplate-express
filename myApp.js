f8f2db55a57dd74b4906f90dee6d88b0391ac40c
Yasmin Askar <hailey.askar@gmail.com>
2022-08-20 05:20:00 -0500

    env config

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 let express = require("express");
 let app = express();
 console.log("Hello World");
+require("dotenv").config();
+
 /*
 app.get("/", function (request, response) {
   response.send("Hello Express");
@@ -16,5 +18,17 @@ app.get("/", (req, res) => {
 const absPath = __dirname + "/public";
 
 app.use("/", express.static(absPath));
+
+let jsObject = { message: "Hello json" };
+
+const mySecret = process.env["MESSAGE_STYLE"];
+
+app.get("/json", (req, res) => {
+  if (mySecret == "uppercase") {
+    jsObject.message = jsObject.message.toUpperCase();
+  }
+
+  res.json(jsObject);
+});
 
 module.exports = app;
