0d1bc7b5a46b6e5b3b226b9c69a79872edeafb2e
Yasmin Askar <hailey.askar@gmail.com>
2022-08-23 08:28:28 -0500

    typo

--- myApp.js
+++ myApp.js
@@ -9,6 +9,7 @@ app.get("/", function (request, response) {
 });
 */
 
+/*
 //placed logger first because express runs in order
 function logger(req, res, next) {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
@@ -17,6 +18,7 @@ function logger(req, res, next) {
 }
 
 app.use("/", logger);
+*/
 
 /*
 let absolutePath = __dirname + "/views/index.html";
